9c6ed54199865c8b85e96047fca251276105043e
i9-7940X haamda <kyoto.snl@gmail.com>
2023-10-31 23:00:24 +0900

    dev

---
+++ _Copilot/Copilot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Copilot
+{
+    internal class Copilot
+    {
+        //サーバーのファイルパス
+        string serverPath = @"\\";
+        //接続できるか
+        bool canConnect = false;
+        //接続できるかの確認
+        public void CheckConnect()
+        {
+            try
+            {
+                System.IO.Directory.GetFiles(serverPath);
+                canConnect = true;
+            }
+            catch
+            {
+                canConnect = false;
+            }
+        }
+        public bool CanConnect
+        {
+            get { return canConnect; }
+        }
+    }
+}
